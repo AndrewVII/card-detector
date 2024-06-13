90d9812db6c8c35488976d3351ad882733304eeb
Andrew Gall <andrewgall12@gmail.com>
2024-06-12 21:11:53 -0700

    Updating for smarter code

--- scripts/card_detector.py
+++ scripts/card_detector.py
@@ -1,24 +1,21 @@
 import torch
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultPredictor
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
 from detectron2 import model_zoo
+from dotenv import load_dotenv
+import os
 
 
-def init_config():
+def init_config(model):
     model_weights_path = "output/model_final.pth"
 
     cfg = get_cfg()
-    cfg.merge_from_file(
-        model_zoo.get_config_file(
-            "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
-        )
-    )
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+    cfg.merge_from_file(model_zoo.get_config_file(model))
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.75
     cfg.MODEL.WEIGHTS = model_weights_path
     cfg.DATASETS.TEST = ("my_dataset",)
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
@@ -31,43 +28,49 @@ def initialize_webcam():
     if not cap.isOpened():
         print("Error: Could not open webcam.")
         exit()
-
     return cap
 
 
-def draw_predictions_on_frame(cap, predictor, cfg):
-    ret, frame = cap.read()
-    if not ret:
-        print("Error: Failed to capture image")
-        return None
-
+def draw_predictions_on_frame(frame, predictor, cfg):
     outputs = predictor(frame)
     v = Visualizer(
         frame[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TEST[0]), scale=1
     )
     out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-
     return out.get_image()[:, :, ::-1]
 
 
 def main():
-    config = init_config()
+    load_dotenv()
+    model_name = os.getenv("MODEL")
+    config = init_config(model_name)
     predictor = DefaultPredictor(config)
     cap = initialize_webcam()
     cur_frame_num = 0
 
     while True:
-        cur_frame = cap.read()[1]
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Failed to capture image")
+            break
+
         if cur_frame_num == 0:
-            cur_frame = draw_predictions_on_frame(cap, predictor, config)
-            if cur_frame is None:
+            result_frame = draw_predictions_on_frame(frame, predictor, config)
+            if result_frame is None:
                 break
-        cv2.imshow("Webcam Detection", cur_frame)
+        else:
+            result_frame = frame
+
+        # Calculate and display FPS
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        print(fps)
+
+        cv2.imshow("Webcam Detection", result_frame)
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
-        cur_frame_num = (cur_frame_num + 1) % 5
+        cur_frame_num = (cur_frame_num + 1) % 1
 
     cap.release()
     cv2.destroyAllWindows()
--- scripts/model_training/train.py
+++ scripts/model_training/train.py
@@ -1,18 +1,20 @@
-import torch
-import detectron2
 from detectron2.engine import DefaultTrainer
 from detectron2.config import get_cfg
 from detectron2.data import DatasetCatalog, MetadataCatalog
 from detectron2.data.datasets import register_coco_instances
 import os
 from detectron2 import model_zoo
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
+    load_dotenv()
     # Path to your dataset directory
     dataset_dir = "dataset/annotations"
+    output_dir = "./output"
+    os.system(f"rm -rf {output_dir}")
 
     # model
-    model_name = "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+    model_name = os.getenv("MODEL")
 
     # Register the dataset
     register_coco_instances(
@@ -28,12 +30,13 @@ if __name__ == "__main__":
     cfg.DATASETS.TRAIN = ("my_dataset",)
     cfg.DATASETS.TEST = ()
     cfg.DATALOADER.NUM_WORKERS = 2
-    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_name)
     cfg.SOLVER.IMS_PER_BATCH = 2
     cfg.SOLVER.BASE_LR = 0.00025
     cfg.SOLVER.MAX_ITER = 1000
     cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # Update this if you have more than one class
+    cfg.MODEL.RETINANET.NUM_CLASSES = 1  # Number of classes in your dataset
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_name)
+    cfg.OUTPUT_DIR = output_dir
 
     # Create output directory if it doesn't exist
     os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
@@ -47,6 +50,6 @@ if __name__ == "__main__":
     from detectron2.evaluation import COCOEvaluator, inference_on_dataset
     from detectron2.data import build_detection_test_loader
 
-    evaluator = COCOEvaluator("my_dataset", cfg, False, output_dir="./output/")
+    evaluator = COCOEvaluator("my_dataset", cfg, False, output_dir=f"{output_dir}/")
     val_loader = build_detection_test_loader(cfg, "my_dataset")
     inference_on_dataset(trainer.model, val_loader, evaluator)
